3e777ff06a7a52cef6e186205d994f8304b66f11
EJ Ozyazgan <ejozyazgan@gmail.com>
2022-04-22 13:17:30 -0700

    Fix Imports

--- src/FunctionComponent.js
+++ src/FunctionComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux'
-import * as actions from './redux/dataSlice';
+import { updateMessage } from './redux/dataSlice';
 
 const FunctionComponent = (props) => {
   
